bcef6515df799eb32d7565b3b490800fec32e09a
jasurumarov <jasurumarov199@gmail.com>
2024-01-16 20:38:30 +0500

    file structure

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ['./*.html', './pages/*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1320px',
+    },
+    extend: {
+      colors: {
+        colorFirst: '#70C174',
+        colorSecond: '#BEF3C0',
+        colorThird: '#EFF7F2',
+        colorFourth: '#0B0706',
+        colorFifth: '#1D2130',
+        colorSixth: '#525560',
+        colorSeventh: '#EBF0F9',
+        colorEighth: '#E5E5E5',
+      },
+    },
+    fontFaimly: {
+      roboto: ['Roboto'],
+      roboto700: ['Roboto'],
+    },
+    fontFaimly: {
+      roboto: ['Roboto700'],
+      roboto700: ['Roboto700'],
+    }
+  },
+  plugins: [],
+}
+
